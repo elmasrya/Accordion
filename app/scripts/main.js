98560440435d176d365060a3412213fe59a69594
Andrew El-Masry <andrewelmasrye@gmail.com>
2014-10-06 11:12:37 -0400

    almost done

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,39 @@
-console.log('The Iron Yard Rocks');
+
+  $('.top').on('click',function() {
+  $('.text').addClass('close');
+  $('this').next().find('.text').removeClass('close');
+  $('this').next().find('.text').addClass('open');
+});
+
+/*
+$('.top2').on('click',function() {
+
+
+  $('.text2').removeClass('text2');
+
+
+
+
+});
+
+$('.top3').on('click',function() {
+
+  $('.text3').removeClass('text3');
+
+});
+
+/*
+$('.top2').click(function() {
+
+
+
+  $('.box2').removeClass('text2');
+});
+
+$('.top3').click(function() {
+
+
+
+  $('.box3').removeClass('text3');
+});
+*/
